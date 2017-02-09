f843b4d699b740e4cf9ae2e2baf2ad448e2548d6
bklynate <nathanielcfa@gmail.com>
2017-02-09 13:27:19 -0500

    [edit] moved to es6 react js & edits to html

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello World! Live from ProductList
+      </div>
+    )
+  }
+}
 
+ReactDOM.render(<ProductList />, document.querySelector('#content'));
